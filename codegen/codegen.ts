f2814b97b5e8267aa4acc388762d50eeffb7b355
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-04-23 21:41:46 -0700

    copy special files from root

--- codegen/codegen.ts
+++ codegen/codegen.ts
@@ -3,7 +3,18 @@ import { generateTubes } from "./tubes.ts";
 import { generateHTML } from "./html.tsx";
 
 if (import.meta.main) {
+  copyFiles();
+  generateFiles();
+}
+
+function copyFiles() {
+  Deno.mkdirSync("generated", { recursive: true });
+  copySync("deno.json", "generated/deno.json", { overwrite: true });
+  copySync("main.ts", "generated/main.ts", { overwrite: true });
   copySync("static", "generated", { overwrite: true });
+}
+
+function generateFiles() {
   generateTubes();
   generateHTML();
 }
