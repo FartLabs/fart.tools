b4fee729d9a66d614601b62b8211ebb03072c0f4
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-04-23 22:25:56 -0700

    wip

---
+++ blog/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: Hello World!
+author: EthanThatOneKid
+date: 2024-04-23
+---
+
+# Hello World!
+
+Epic blog post content here!
--- codegen/html.tsx
+++ codegen/html.tsx
@@ -3,4 +3,10 @@ import { LandingPage } from "#/components/landing_page/mod.ts";
 export function generateHTML() {
   const landingPageHTML = <LandingPage /> as string;
   Deno.writeTextFileSync("generated/index.html", landingPageHTML);
+
+  // TODO: Generate blog page, post pages, and label filtered pages.
 }
+
+// /topics/deno/index.html
+// /topics/index.html
+// /post/index.html
