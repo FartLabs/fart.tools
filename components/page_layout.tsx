0e18a75367ce1de751c6a9ab345d3e9a8c84b6c2
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-27 10:41:32 -0700

    Update page_layout.tsx

--- components/page_layout.tsx
+++ components/page_layout.tsx
@@ -58,7 +58,7 @@ function Favicon() {
 
 const stylesheets = [
   "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark-dimmed.min.css",
-  "https://css.fart.tools",
+  "https://css.fart.tools/fart.css",
   "/background-blob.css",
   "/tube-green.css",
   "/tube-purple.css",
