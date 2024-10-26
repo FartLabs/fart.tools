4ffddf8ca91b5b5064140244ab2bc5be99642c79
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-26 15:40:59 -0700

    add dynamic directory name to html codegen

--- codegen/html.tsx
+++ codegen/html.tsx
@@ -7,29 +7,29 @@ import {
 } from "#/components/blog_page/mod.ts";
 import { BlogPostPage } from "#/components/blog_page/blog_post_page/mod.ts";
 
-export function generateHTML() {
+export function generateHTML(directory: string = "generated") {
   const landingPageHTML: string = <LandingPage />;
-  Deno.writeTextFileSync("generated/index.html", landingPageHTML);
+  Deno.writeTextFileSync(`${directory}/index.html`, landingPageHTML);
 
   const blogPageHTML: string = <BlogPage />;
-  Deno.mkdirSync("generated/blog", { recursive: true });
-  Deno.writeTextFileSync("generated/blog/index.html", blogPageHTML);
+  Deno.mkdirSync(`${directory}/blog`, { recursive: true });
+  Deno.writeTextFileSync(`${directory}/blog/index.html`, blogPageHTML);
 
   for (const topic of topics) {
     const topicID = toTopicID(topic);
     const topicPageHTML: string = <BlogPage topicID={topicID} />;
-    Deno.mkdirSync(`generated/blog/${topicID}`, { recursive: true });
+    Deno.mkdirSync(`${directory}/blog/${topicID}`, { recursive: true });
     Deno.writeTextFileSync(
-      `generated/blog/${topicID}/index.html`,
+      `${directory}/blog/${topicID}/index.html`,
       topicPageHTML,
     );
   }
 
   for (const post of posts) {
     const postPageHTML: string = <BlogPostPage post={post} />;
-    Deno.mkdirSync(`generated/${post.id}`, { recursive: true });
+    Deno.mkdirSync(`${directory}/${post.id}`, { recursive: true });
     Deno.writeTextFileSync(
-      `generated/${post.id}/index.html`,
+      `${directory}/${post.id}/index.html`,
       postPageHTML,
     );
   }
