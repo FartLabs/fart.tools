ac6181bc646b7212062396991e01b5d962c8d790
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-28 17:22:37 -0700

    landing page: add foreword section

--- components/landing_page/landing_page.tsx
+++ components/landing_page/landing_page.tsx
@@ -1,5 +1,6 @@
 import { PageLayout } from "#/components/page_layout.tsx";
 import { HeroSection } from "./hero_section.tsx";
+import { ForewordSection } from "./foreword_section.tsx";
 import { ProjectsSection } from "./projects_section.tsx";
 import { RiseSection } from "./rise_section.tsx";
 import { FAQsSection } from "./faqs_section.tsx";
@@ -8,6 +9,7 @@ export function LandingPage() {
   return (
     <PageLayout>
       <HeroSection />
+      <ForewordSection />
       <ProjectsSection />
       <RiseSection />
       <FAQsSection />
