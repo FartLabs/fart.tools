7c261de79f395b6b317f354a8ae514f0507d6117
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-29 21:34:27 -0700

    implement ga4
    Reference: <https://jsr.io/@kitsonk/ga4@0.1.0/doc/~/GA4Report>

--- google-analytics.ts
+++ google-analytics.ts
@@ -1,8 +1,6 @@
-import { createReporter } from "ga";
+import { GA4Report } from "@kitsonk/ga4";
 
 const GOOGLE_ANALYTICS_ID = Deno.env.get("GOOGLE_ANALYTICS_ID")!;
-
-const ga = createReporter({ id: GOOGLE_ANALYTICS_ID });
 
 export function useGoogleAnalytics(
   fn: (
@@ -12,19 +10,21 @@ export function useGoogleAnalytics(
 ) {
   return async function (
     request: Request,
-    info: Deno.ServeHandlerInfo<Deno.NetAddr>,
+    conn: Deno.ServeHandlerInfo<Deno.NetAddr>,
   ) {
-    let error;
     let response: Response;
-    const start = performance.now();
     try {
-      response = await fn(request, info);
-    } catch (e) {
-      error = e;
+      response = await fn(request, conn);
     } finally {
-      ga(request, info, response!, start, error);
+      const report = new GA4Report({
+        measurementId: GOOGLE_ANALYTICS_ID,
+        conn,
+        request,
+        response: response!,
+      });
+      await report.send();
     }
 
-    return response!;
+    return response;
   };
 }
